936d121be1f8603cb043a8f9e850bec237b7456e
CalebKarr1 <s123328@ltisdschools.net>
2025-04-15 14:02:08 -0500

    fixed player movement file

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,14 +9,25 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     AttributeManager attributes;
     public float maxSpeed = 10f;
+
+    public float localmaxSpeed = 1f;
+
+    public float distanceToMouseLimit = 400;
     public GameObject[] boosterObjects;
     private float boosterLevel = 0f;
+
+
+
+    public ParticleSystem smoke;
     void Start()
     {
+
         foreach(GameObject booster in boosterObjects)
         {
-            //booster.transform.localScale = new Vector3(boosterLevel, boosterLevel, boosterLevel);
-            Debug.Log(booster.transform.localScale);
+            
+            booster.transform.localScale = new Vector3(booster.transform.localScale.x, boosterLevel, booster.transform.localScale.z);
+
+            
         }
         
         attributes = GetComponent<AttributeManager>();
@@ -37,14 +48,17 @@ public class PlayerMovement : MonoBehaviour
 
         // move ship
         float forward = Input.GetAxis("Vertical");
-
+        //Debug.Log("Forward: " + forward);
         if (forward > 0f)
         {
-            boosterLevel += 0.1f;
+            boosterLevel += 0.05f;
+            smoke.Play();
+
         }
         else
         {
-            boosterLevel =0f;
+            boosterLevel -= 0.1f;
+            smoke.Stop();
         }
         boosterLevel = Mathf.Min(boosterLevel, 1);
 
@@ -54,17 +68,23 @@ public class PlayerMovement : MonoBehaviour
         {
             booster.transform.localScale = new Vector3(boosterLevel, boosterLevel, boosterLevel);
         }
-
+        Vector3 objectSreenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mouseScreenPos = Input.mousePosition;
+        float distanceToMouse = Vector3.Distance(objectSreenPos, mouseScreenPos);
+        Debug.Log("Distance to mouse: " + distanceToMouse);
+        localmaxSpeed = Mathf.Min(distanceToMouse/distanceToMouseLimit,1)*maxSpeed;
+        //Debug.Log("Local max speed: " + localmaxSpeed);
+        //Debug.Log("max speed: " + maxSpeed);
         rb.AddForce(transform.up * 2f * forward, ForceMode2D.Force);
 
     }
     void FixedUpdate()
     {
         //Debug.Log("Speed: " + rb.velocity.magnitude + " max: " + maxSpeed.getvalue());
-        if (rb.velocity.magnitude > maxSpeed)
+        if (rb.velocity.magnitude > localmaxSpeed)
         {
             // Clamp the velocity magnitude
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+            rb.velocity = rb.velocity.normalized * localmaxSpeed;
         }
     }
 }
