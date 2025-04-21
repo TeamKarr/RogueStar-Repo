9e7e5a074c4f498599fb1e0a96cc9fdaf37519db
JackWallander <s120814@ltisdschools.net>
2025-04-21 16:04:56 -0500

    added projectiles

---
+++ Assets/Scripts/Projectiles/ShootingController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A class which controlls player aiming and shooting
+/// </summary>
+public class ShootingController : MonoBehaviour
+{
+    [Header("GameObject/Component References")]
+    [Tooltip("The projectile to be fired.")]
+    public GameObject projectilePrefab = null;
+    [Tooltip("The transform in the heirarchy which holds projectiles if any")]
+    public Transform projectileHolder = null;
+
+    [Header("Input")]
+    [Tooltip("Whether this shooting controller is controled by the player")]
+    public bool isPlayerControlled = false;
+
+    [Header("Firing Settings")]
+    [Tooltip("The minimum time between projectiles being fired.")]
+    public float fireRate = 0.05f;
+
+    [Tooltip("The maximum diference between the direction the" +
+        " shooting controller is facing and the direction projectiles are launched.")]
+    public float projectileSpread = 1.0f;
+
+    // The last time this component was fired
+    private float lastFired = Mathf.NegativeInfinity;
+
+    [Header("Effects")]
+    [Tooltip("The effect to create when this fires")]
+    public GameObject fireEffect;
+
+    [Header("Bullet Spawnpoint")]
+    public Transform spawnpoint;
+
+    /// <summary>
+    /// Description:
+    /// Standard unity function that runs every frame
+    /// Inputs:
+    /// none
+    /// Returns:
+    /// void (no return)
+    /// </summary>
+    private void Update()
+    {
+        ProcessInput();
+    }
+    private void LateUpdate()
+    {
+        if (!isPlayerControlled)
+        {
+            Fire();
+        }
+    }
+
+    /// <summary>
+    /// Description:
+    /// Reads input from the input manager
+    /// Inputs:
+    /// None
+    /// Returns:
+    /// void (no return)
+    /// </summary>
+    void ProcessInput()
+    {
+        if (isPlayerControlled)
+        {
+            if (Input.GetMouseButton(0) == true)
+            {
+                Fire();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Description:
+    /// Fires a projectile if possible
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    public void Fire()
+    {
+        // If the cooldown is over fire a projectile
+        if ((Time.timeSinceLevelLoad - lastFired) > fireRate)
+        {
+            // Launches a projectile
+            SpawnProjectile();
+
+            if (fireEffect != null)
+            {
+                Instantiate(fireEffect, transform.position, transform.rotation, null);
+            }
+
+            // Restart the cooldown
+            lastFired = Time.timeSinceLevelLoad;
+        }
+    }
+
+    /// <summary>
+    /// Description:
+    /// Spawns a projectile and sets it up
+    /// Inputs: 
+    /// none
+    /// Returns: 
+    /// void (no return)
+    /// </summary>
+    public void SpawnProjectile()
+    {
+        // Check that the prefab is valid
+        if (projectilePrefab != null)
+        {
+            Vector3 pos = spawnpoint.position;
+            // Create the projectile
+            GameObject projectileGameObject = Instantiate(projectilePrefab, pos, transform.rotation, null);
+            // Account for spread
+            Vector3 rotationEulerAngles = projectileGameObject.transform.rotation.eulerAngles;
+            rotationEulerAngles.z += Random.Range(-projectileSpread, projectileSpread);
+            projectileGameObject.transform.rotation = Quaternion.Euler(rotationEulerAngles);
+
+            // Keep the heirarchy organized
+            if (projectileHolder != null)
+            {
+                projectileGameObject.transform.SetParent(projectileHolder);
+            }
+        }
+    }
+}
