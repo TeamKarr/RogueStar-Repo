1f1e4e88150517b9a889e724b2b3970c1d31b527
JoshIsCode <s123325@ltisdschools.net>
2025-05-01 12:07:17 -0500

    finalized mechanic upgrading system

--- Assets/Scripts/UI/UpgraderManager.cs
+++ Assets/Scripts/UI/UpgraderManager.cs
@@ -25,7 +25,7 @@ public class UpgraderManager : MonoBehaviour
     {
         //float amount = manager.GetAttribute(upgradeName);\
         Debug.Log(manager);
-        upgrade = manager.defaultAttributes["MaxSpeed"];
+        upgrade = manager.defaultAttributes[upgradeName];
         titleText = title.text;
         slider.minValue = upgrade.amount;
         slider.maxValue = upgrade.amount + upgrade.UpgradeAmount*upgrade.MaxUpgrades;
@@ -39,18 +39,38 @@ public class UpgraderManager : MonoBehaviour
         Debug.Log("updated amount");
         slider.value = upgrade.amount;
         title.text = titleText + " " + upgrade.amount.ToString("F1");
-        buttonLabel.text = "UPGRADE\n(" + upgrade.UpgradeAmount.ToString("F1") + " Gold)";
+        buttonLabel.text = "UPGRADE\n(" + upgrade.Cost.ToString("F1") + " M)";
+        if (upgrades >= upgrade.MaxUpgrades)
+        {
+            buttonLabel.text = "MAX\nUPGRADES";
+            button.interactable = false;
+        }
+        else if (manager.Matter < upgrade.Cost)
+        {
+            buttonLabel.color = Color.red;
+            button.interactable = false;
+        }
+        else
+        {
+            buttonLabel.color = Color.black;
+            button.interactable = true;
+        }
     }
 
     public void Upgrade()
     {
+        if (manager.Matter < upgrade.Cost)
+        {
+            Debug.Log("Not enough matter");
+            updateDisplay();
+            return;
+        }
+        manager.Matter -= (int)upgrade.Cost;
+        upgrade.Cost += upgrade.CostIncrease;
         upgrade.amount += upgrade.UpgradeAmount;
         upgrade.amount = Mathf.Clamp(upgrade.amount, slider.minValue, slider.maxValue);
         upgrades++;
-        if (upgrades >= upgrade.MaxUpgrades)
-        {
-            button.interactable = false;
-        }
+        
         updateDisplay();
     }
 
