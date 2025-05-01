656057ab1e5fd3d6e2ec9869a748ea5b1fb2d917
JackWallander <s120814@ltisdschools.net>
2025-05-01 15:52:41 -0500

    made it so exp orbs move toward the player

--- Assets/Scripts/Enemies/ExperienceOrb.cs
+++ Assets/Scripts/Enemies/ExperienceOrb.cs
@@ -7,12 +7,54 @@ public class ExperienceOrb : MonoBehaviour
     public string expAttribute = "Experience";
     Attribute experience;
 
+    Rigidbody2D orb;//hehe thats funny(it stands for orb rigid body)
+    public float maxSpeed;
+    private GameObject player;
+    private Vector2 rbpPosition, rboPosition;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         experience = (GetComponent<AttributeManager>()).getAttribute(expAttribute);
+        orb = GetComponent<Rigidbody2D>();
+
+        //find the player object(not prefab)
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.layer == 3)
+            {
+                player = obj;
+            }
+        }
+
+    }
+
+
+
+    void Update()
+    {
+
+        //player position
+        rbpPosition = player.transform.position;
+        rboPosition = orb.position;
+        //look at player
+        transform.Rotate(new Vector3(0, 0, 1), 1);
+        Vector3 direction = ((rbpPosition - rboPosition)).normalized;
+        transform.up = direction;
+
+        //move the orb
+        orb.AddForce(transform.up * .25f, ForceMode2D.Force);
+
+        //max speed
+        Vector2 velocity = orb.velocity;
+        float currentSpeed = velocity.magnitude;
+
+        if (currentSpeed > maxSpeed)
+        {
+            Vector2 normalizedVelocity = velocity.normalized;
+
+            orb.velocity = normalizedVelocity * maxSpeed;
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
